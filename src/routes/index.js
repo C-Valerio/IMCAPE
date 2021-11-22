4315a5bcee1866608c7dbaf59700df22c5d5ffa1
naerzone2404 <90951593+naerzone2404@users.noreply.github.com>
2021-11-22 00:25:25 -0500

    actualizacion

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,7 +5,7 @@ const router = express.Router()
 // const client = require('../libs/connect')()
 
 router.get('/contact',(req,res)=>{
-    res.render('contact')
+    res.render('contact.html')
 })
 
 router.get('/', (req,res) => {
@@ -13,17 +13,16 @@ router.get('/', (req,res) => {
     
 })
 
-router.get('/reporte',(req,res) =>{
-    res.send("<h1>Pagina del Reporte</h1>")
+router.get('/producto',(req,res) =>{
+    res.send("producto")
+})
+router.get('/services',(req,res) =>{
+    res.send("services")
+})
+router.get('/services',(req,res) =>{
+    res.send("services")
 })
 
-router.post('/consultar',(req,res) =>{
-    res.send("{frutas:['manzana','pera','fresa']}")
-})
-
-router.delete('/eliminar',(req,res) =>{
-    res.send("Eliminado")
-})
 
 router.post('/insertar',(req,res)=>{
     client.connect(async(err)=>{
@@ -38,42 +37,42 @@ router.post('/insertar',(req,res)=>{
    // res.send(req.body.curso)//
 })
 
-router.post('/listarAlumnos',(req,res) =>{
-    client.connect(async (err) => {
-        if(!err){
-            const collection = client.db("test").collection("alumno")
-            collection.find().toArray((err,result)=>{
-                if(!err){
-                    //res.send(result)
-                    res.render('listarAlumnos',{datos:result})
-                }else{
-                    res.send("'resultado':[{'respuesta':'Error al traer la data'},{'mensaje':" + err + "}]")
-                }
-            })  
-        }else{
-            res.send("resultado:[{'respuesta':'Error al conectar a DB'},{'mensaje':" + err + "}]")
-        }
-    })
-})
+// router.post('/listarAlumnos',(req,res) =>{
+//     client.connect(async (err) => {
+//         if(!err){
+//             const collection = client.db("test").collection("alumno")
+//             collection.find().toArray((err,result)=>{
+//                 if(!err){
+//                     //res.send(result)
+//                     res.render('listarAlumnos',{datos:result})
+//                 }else{
+//                     res.send("'resultado':[{'respuesta':'Error al traer la data'},{'mensaje':" + err + "}]")
+//                 }
+//             })  
+//         }else{
+//             res.send("resultado:[{'respuesta':'Error al conectar a DB'},{'mensaje':" + err + "}]")
+//         }
+//     })
+// })
 
-router.post('/listarUnAlumno',(req,res) =>{     //console.log(nombre)
-    var nombreLocal = req.body.nombre;
-    client.connect(async (err) => {
-        if(!err){
-            const collection = client.db("test").collection("alumno")
-            collection.find({nombre:{$ed:nombreLocal}}).toArray((err,result)=>{
-                if(!err){
-                    //res.send(result)
-                    res.render('listarAlumnos',{datos:result})
-                }else{
-                    res.send("'resultado':[{'respuesta':'Error al traer la data'},{'mensaje':" + err + "}]")
-                }
-            })  
-        }else{
-            res.send("resultado:[{'respuesta':'Error al conectar a DB'},{'mensaje':" + err + "}]")
-        }
-    })
-})
+// router.post('/listarUnAlumno',(req,res) =>{     //console.log(nombre)
+//     var nombreLocal = req.body.nombre;
+//     client.connect(async (err) => {
+//         if(!err){
+//             const collection = client.db("test").collection("alumno")
+//             collection.find({nombre:{$ed:nombreLocal}}).toArray((err,result)=>{
+//                 if(!err){
+//                     //res.send(result)
+//                     res.render('listarAlumnos',{datos:result})
+//                 }else{
+//                     res.send("'resultado':[{'respuesta':'Error al traer la data'},{'mensaje':" + err + "}]")
+//                 }
+//             })  
+//         }else{
+//             res.send("resultado:[{'respuesta':'Error al conectar a DB'},{'mensaje':" + err + "}]")
+//         }
+//     })
+// })
 
 module.exports = router;
 

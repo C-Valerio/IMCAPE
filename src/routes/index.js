37855427cae2ebb73f267309a8b5b588734e2e33
naerzone2404 <90951593+naerzone2404@users.noreply.github.com>
2021-11-27 01:55:33 -0500

    aplicando insercion

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,17 +2,14 @@ const { render } = require('ejs')
 const fs = require('fs')
 const express = require('express')
 const router = express.Router()
-// const client = require('../libs/connect')()
+const client = require('../libs/connect')()
 
 router.get('/', (req,res) => {
     res.render('index') 
 })
 router.get('/contacto',(req,res)=>{
     res.render('contacto')
-    
-
 })
-
 router.get('/productos',(req,res) =>{
     res.render("productos")
 })
@@ -23,7 +20,10 @@ router.get('/servicios',(req,res) =>{
     res.render("servicios")  
 })
 router.post('/insertar', (req,res) =>{
-    res.send(req.body.nombre)
+    client.connect(async (err)=>{
+        const collection = client.db("test").collection("alumno")
+        collection.insertOne(req.body)
+    })
 })
 
 module.exports = router;

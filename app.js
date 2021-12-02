78f33d2bc57189c0f485d58c1732dc4f5f106648
C-Valerio <81839625+C-Valerio@users.noreply.github.com>
2021-12-02 01:07:10 -0500

    avance6

--- app.js
+++ app.js
@@ -1,24 +1,23 @@
-const path = require ("path")
-const express =require("express")
-const logger = require ("morgan")
+const path = require("path")
+const express = require("express")
+const logger = require("morgan")
 const bodyParse = require("body-parser")
 const app = express()
 const indexRoutes = require("./src/routes/index")
 
+app.use(express.static('src'))
+app.use(express.static('src/libs'))
 
-app.set("port",process.env.PORT || 3000)
-app.set("views",path.join(__dirname,"src/views"))
-app.set("view engine","ejs")
-
-app.use(express.static(__dirname + "src/libs"))
-
+app.set("port", process.env.PORT || 3000)
+app.set("views", path.join(__dirname, "src/views"))
+app.set("view engine", "ejs")
 
 app.use(logger("dev"))
 app.use(express.json())
-app.use(bodyParse.urlencoded({extended:false}))
+app.use(bodyParse.urlencoded({ extended: false }))
 
-app.use("/",indexRoutes)
+app.use("/", indexRoutes)
 
-app.listen(app.get("port"),() => {
-    console.log("Servidor en Puerto",app.get("port"))
+app.listen(app.get("port"), () => {
+    console.log("Servidor en Puerto", app.get("port"))
 })

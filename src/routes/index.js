236553b10789b79c8cb0e00dda0352b7a2a1522a
C-Valerio <81839625+C-Valerio@users.noreply.github.com>
2021-12-10 22:53:32 -0500

    avance8

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,78 +6,81 @@ const router = express.Router()
 const client = require('../libs/connect')()
 
 
-router.get('/', (req,res) => {
-    res.render('index') 
+router.get('/', (req, res) => {
+    res.render('index')
 })
-router.get('/index', (req,res) => {
-    res.render('index') 
+router.get('/index', (req, res) => {
+    res.render('index')
 })
-router.get('/contacto',(req,res)=>{
+router.get('/contacto', (req, res) => {
     res.render('contacto')
 })
-router.get('/productos',(req,res) =>{
+router.get('/productos', (req, res) => {
     res.render("productos")
 })
-router.get('/noticias',(req,res) =>{
+router.get('/carrito', (req, res) => {
+    res.render("carrito")
+})
+router.get('/noticias', (req, res) => {
     res.render("noticias")
 })
-router.get('/servicios',(req,res) =>{
-    res.render("servicios")  
-})
-// router.post('/insertar', (req,res) =>{
-//     client.connect(async (err)=>{
-//         if(!err){
-//         const collection = client.db("test").collection("alumno")
-//         collection.insertOne(req.body)
-//         res.send("resultado:[{'respuesta':'ok'}]")
-//     } else{
-//         res.send("resultado:[{'respuesta':'Error al cargar'},{'mensaje':" + err +"}]")}
-//     })
-// })
+router.get('/servicios', (req, res) => {
+        res.render("servicios")
+    })
+    // router.post('/insertar', (req,res) =>{
+    //     client.connect(async (err)=>{
+    //         if(!err){
+    //         const collection = client.db("test").collection("alumno")
+    //         collection.insertOne(req.body)
+    //         res.send("resultado:[{'respuesta':'ok'}]")
+    //     } else{
+    //         res.send("resultado:[{'respuesta':'Error al cargar'},{'mensaje':" + err +"}]")}
+    //     })
+    // })
 
-router.post('/contacto', (req,res) =>{
-    client.connect(async (err)=>{
-        if(!err){
-        const collection = client.db("test").collection("alumno")
-        collection.insertOne(req.body)
-        res.render("contacto")
-    } else{
-        res.send("resultado:[{'respuesta':'Error al cargar'},{'mensaje':" + err +"}]")}
+router.post('/contacto', (req, res) => {
+    client.connect(async(err) => {
+        if (!err) {
+            const collection = client.db("test").collection("alumno")
+            collection.insertOne(req.body)
+            res.render("contacto")
+        } else {
+            res.send("resultado:[{'respuesta':'Error al cargar'},{'mensaje':" + err + "}]")
+        }
     })
 })
-router.post('/listarAlumnos', (req,res) =>{
-    client.connect(async (err)=>{
-        if(!err){
-        const collection = client.db("test").collection("alumno")
-        collection.find().toArray((err, result)=>{
-            if(!err){
-                res.send(result)
-                // res.render('/contacto', {datos:result})
-            }else{
-                res.send("'resultado':[{'respuesta':'Error al cargar'},{'mensaje':" + err +"}]")
+router.post('/listarAlumnos', (req, res) => {
+        client.connect(async(err) => {
+            if (!err) {
+                const collection = client.db("test").collection("alumno")
+                collection.find().toArray((err, result) => {
+                    if (!err) {
+                        res.send(result)
+                            // res.render('/contacto', {datos:result})
+                    } else {
+                        res.send("'resultado':[{'respuesta':'Error al cargar'},{'mensaje':" + err + "}]")
+                    }
+                })
+            } else {
+                res.send("resultado:[{'respuesta':'Error al cargar'},{'mensaje':" + err + "}]")
             }
         })
-    } else{
-        res.send("resultado:[{'respuesta':'Error al cargar'},{'mensaje':" + err +"}]")
-    }
     })
-})
-// router.post('/contacto', (req,res) =>{
-//     client.connect(async (err)=>{
-//         if(!err){
-//         const collection = client.db("test").collection("alumno")
-//         collection.find().toArray((err,result)=>{
-//             if(!err){
-//                 res.render('contacto',{datos:result})
-//             }else{
-//                 res.send("'resultado':[{'respuesta':'Error al cargar'},{'mensaje':" + err +"}]")
-//             }
-//         })
-//     } else{
-//         res.send("resultado:[{'respuesta':'Error al cargar'},{'mensaje':" + err +"}]")
-//     }
-//     })
-// })
+    // router.post('/contacto', (req,res) =>{
+    //     client.connect(async (err)=>{
+    //         if(!err){
+    //         const collection = client.db("test").collection("alumno")
+    //         collection.find().toArray((err,result)=>{
+    //             if(!err){
+    //                 res.render('contacto',{datos:result})
+    //             }else{
+    //                 res.send("'resultado':[{'respuesta':'Error al cargar'},{'mensaje':" + err +"}]")
+    //             }
+    //         })
+    //     } else{
+    //         res.send("resultado:[{'respuesta':'Error al cargar'},{'mensaje':" + err +"}]")
+    //     }
+    //     })
+    // })
 
 module.exports = router;
-
